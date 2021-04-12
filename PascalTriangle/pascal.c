76b8654b401c09430ed6baf6b61ee20e788f2888
2000594 <e2000594@shell.vamk.fi>
2021-04-12 17:10:28 +0300

    Coments added to pascal.c

--- PascalTriangle/pascal.c
+++ PascalTriangle/pascal.c
@@ -2,6 +2,10 @@
 #include <math.h>
 
 
+/**
+ * @brief This function is for generating and printing Paskal triangle
+ * @param[in] v - int value represents number of rows
+ * /
 void parscal_triangle(int v) {
 	int i, j;
 	for (i = 0; i < v; i++) {
@@ -21,12 +25,27 @@ void parscal_triangle(int v) {
 	}
 }
 
+
+/**
+ *@brief Function to obtain number of (n,m) combinations.
+ * The elements of the triangle can be filled as combinations (n = row, m - element in the row)
+ * @param[in] n - int value, row number (starts from 0)
+ * @param[in] m - int value, element in the row (starts from 0)
+ * @return - long long int, number of combinations
+ * /
 long long int comb(int n, int m) {
 	if (n < m) return 0;
 	return fact(n) / (fact(n - m) * fact(m));
 }
 
 
+/**
+ *@brief This function is asking user to enter the number which represents an amount of rows in Paskal triangle
+ *in range from min to max
+ *@param[in] min - int value, minimum positiv number, 0 is the lower limit
+ *@param[in] max - int value, maximum positive number to choose.
+ *@return - int value, choosen number
+ */
 int get_int(int min, int max) {
 	int t;
 	do {
@@ -38,7 +57,14 @@ int get_int(int min, int max) {
 	return t;
 }
 
-long long int fact(int num) {
+
+
+/**
+ *@brief Factorial calculation function
+ *@param[in] num - int value, number factorial of which you want to obtain
+ *@return - long long int, result of factorial calculations
+ * /
+long long int fact(int num) {i
 	long long int result = 1;
 	for (int i = 1; i <= num; i++)
 	{
